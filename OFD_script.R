0e390421b8a0f1c73c426bc924e9cb0ca74eb88d
joyobrien20 <joymichob@gmail.com>
2022-10-10 19:20:14 -0400

    Early beginnings of OFD analysis on the IN pond data between active and total communities.

--- OFD_script.R
+++ OFD_script.R
@@ -10,48 +10,92 @@
 library(vegan)
 library(dplyr)
 library(readxl)
-
-
+library(writexl)
+library(tidyverse)
+library(ggplot2)
 
 # Convert to presence-absence matrix. 
 # Presence absence for cDNA and DNA combined is the PondsPA dataset (INPond_Initia.RData), prettry sure its pondsPA
 
-# We probably need to separate cDNA from DNA here
-# use subset function?
+# Subset the data based on active community and total community
+# cDNA
+active_PA <- PondsPA[grep('-cDNA', rownames(PondsPA)),]
 
-# Read in the active community PA table 
-PondsPA_active <- subset(PondsPA, rownames(PondsPA %in% "-cDNA"))
+# DNA
+total_PA <- PondsPA[grep('-DNA', rownames(PondsPA)),]
 
-#PondsPA_total <- subset(PondsPA, rownames(PondsPA %in% "DNA")) #trying to get this line of code to work; subset must be logical error
-PondsPA_total <- read_excel#(insert file path here)
+# save these data matrices 
+#saveRDS(active_PA,"~/Desktop/IU/Lennon Lab/DormancyDecay/active_PA.rds")
+#saveRDS(total_PA, "~/Desktop/IU/Lennon Lab/DormancyDecay/total_PA.rds")
 
 # For each row (species), we would take the sum across sites (columns), which would result in a vector that 
 #could be used to make frequency histograms: one for DNA and one for RNA. 
 
 # Transpose the data
-PondsPA_active_t <- t(PondsPA_active)
-PondsPA_total_t <- t(PondsPA_total)
+PondsPA_active_t <- t(active_PA)
+PondsPA_total_t <- t(total_PA)
 
-# Export the transposed data and re-import to get the right format (can't figure it out in R!)
-write.csv(PondsPA_active_t,"~/Desktop/PondsPA_active_t.csv")
-write.csv(PondsPA_total_t, "~/Desktop/PondsPA_total_t.csv")
-#Delete the added row at the top of each data frame 
-# PondsPA_active_t <- PondsPA_active_t[-1,] this line of code doesn't work, so I did it manually (files "PondsPA_active_OFD.csv" and "PondsPA_total_OFD.csv")
+# save these transposed matrices here
+saveRDS(PondsPA_total_t, "~/Desktop/IU/Lennon Lab/DormancyDecay/PondsPA_total_t.rds")
+saveRDS(PondsPA_active_t, "~/Desktop/IU/Lennon Lab/DormancyDecay/PondsPA_active_t.rds")
+
+#*******************************************************************************************
 
 # Step 1: Sum across sites (columns)
 # For active community
-active <- colSums(PondsPA_active_OFD[,-1]) 
+active <- colSums(PondsPA_active_t[,-1]) 
 summary(active)
+print(active)
+
 qqnorm(active)
-histogram(active)
+hist(active)
+
+# for total community 
+total <- colSums(PondsPA_total_t[,-1])
+print(total)
+
+qqnorm(total)
+hist(total)
+
+# Step 2: Divide col sums RNA/n row RNA
+nrow(PondsPA_active_t)
+active_OFD <- active/34059 #34059 is the number of OTUs which is the number of rows in the site by species matrix 
+print(active_OFD)
+
+hist(active_OFD)
 
 # For total community 
-total <- colSums(PondsPA_total_OFD[,-1])
-summary(total)
+nrow(PondsPA_total_t)
+total_OFD <- total/34059
+summary(total_OFD)
+print(total)
+
 qqnorm(total)
-histogram(total)
+hist(total_OFD)
+plot(total)
+barplot(active_OFD)
+barplot(total_OFD)
 
 
+# Now divide col sums DNA/nrow DNA 
+total_OFD <- total/16383 #16383 is the number of OTUs
+print(DNA_comp)
+summary(DNA_comp)
+summary(RNA_comp)
+hist(DNA_comp)
+
+# Write the DNA and RNA comps to merge in excel 
+write.csv(total_OFD, "~/Desktop/total_OFD.csv")
+write.csv(active_OFD, "~/Desktop/active_OFD.csv")
+
+
+# Read in data for OFD
+
+
+# https://rdrr.io/rforge/vegan/man/MOStest.html
+test <- MOStest(DNA_comp, RNA_comp) #this isnt working because there are more samples for DNA than RNA #this is a whole problem where I need to start from sq.1
+# Visualize the relationship 
+# Merge the values?
 # We could then statistically compare by 
 # fitting models or some other approach. I could imagine that we might also be able to do some resampling of the data. 
 #We might want to control for differences in species richness. Perhaps this could be done by relativizing y-axis so it’s % species in the DNA or RNA pool. But we could also consider bootstrapping, either by species or sites. 
@@ -62,7 +106,14 @@ histogram(total)
 # Just compare colSums(DNA)/nrow(DNA) to colSums(RNA)/nrow(RNA) on your site by species matrix
 #nrow = the number of rows 
 
+# Figure out how to merge this with Git- https://stackoverflow.com/questions/57637795/fatal-could-not-read-username-when-pushing-to-git-using-a-cron-job
 
 # Performed with the "MOStest" function in the vegan package with a presence/absence OTU table 
-test <- MOStest(active, total)
+test <- MOStest(total_OFD_amend_valonly, active_OFD_valonly)
+plot(total_OFD_amend_valonly)
+plot(active_OFD_valonly)
+
+# trial
+
+
 test <- MOStest(log(active), , family=quasipoisson())
